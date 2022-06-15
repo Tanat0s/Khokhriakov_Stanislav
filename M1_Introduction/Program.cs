e724f8efd613a462d55ea4f8776b028b0595b8a7
Tanat0s <berserk.bellato@yandex.ru>
2022-06-15 23:22:29 +0300

    Created 2 solutions for 1st module.

---
+++ M1_Introduction/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace M1_Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1
+            Console.WriteLine("Hello World!!!");
+            //Задание 2
+            Console.Write("Hello ");
+            Console.Write("World");
+            Console.Write("!!!");
+
+            Console.ReadKey();
+        }
+    }
+}
