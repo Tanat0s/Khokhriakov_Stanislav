4cd02ff96c3130e1e1de069aaa8f03f3e3dd0f33
Tanat0s <berserk.bellato@yandex.ru>
2022-06-25 20:49:06 +0300

    M3: Added 3 solutions for 3 task

---
+++ M3_Cycles/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace M3_1_Odd_Even
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число:");
+            var number = int.Parse(Console.ReadLine());
+            var result = (number % 2 == 0) ? "чётное" : "нечётное";
+
+            Console.WriteLine($"Число {number} {result}.");
+        }
+    }
+}
