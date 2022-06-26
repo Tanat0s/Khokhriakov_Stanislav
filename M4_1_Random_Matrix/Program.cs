9779ed07b0cde7fa4718f4b4aa58e4f916c55763
Tanat0s <berserk.bellato@yandex.ru>
2022-06-26 12:38:55 +0300

    M4: Added 3 solutions of 4 module

---
+++ M4_1_Random_Matrix/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace M4_1_Random_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Для создания матрицы введи необходимое число строк:");
+            var rowsNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введи необходимое число столбцов:");
+            var columnsNumber = int.Parse(Console.ReadLine());
+
+            int[,] array2d = new int[rowsNumber, columnsNumber];
+            var random = new Random();
+            var result = 0;
+
+            for(var i = 0; i < array2d.GetLength(0); i++)
+                for (int j = 0; j < array2d.GetLength(1); j++)
+                {
+                    array2d[i, j] = random.Next(0, 100);
+                }
+
+            for (var i = 0; i < array2d.GetLength(0); i++)
+            {
+                for (int j = 0; j < array2d.GetLength(1); j++)
+                {
+                    Console.Write($"{array2d[i, j], 3} ");
+                    result += array2d[i, j];
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"Сумма всех элементов матрицы: {result}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ M4_2_Min_Element/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace M4_2_Min_Element
+{
+    //Условие задачи.
+    /*Найдите наименьший элемент в последовательности, которую вводит пользователь. Последовательность нужно сохранить в массив. Детальный алгоритм программы:
+        1.Пользователь вводит длину последовательности. 
+        2.Затем пользователь последовательно вводит целые числа (как положительные, так и отрицательные). Числа разделяются клавишей Enter.
+        3.Каждое введённое число помещается в соответствующий элемент массива.
+        4.После окончания ввода данных отдельный цикл проходит по последовательности и находит минимальное число. Для этого он сравнивает каждое число в итерации с предыдущим найденным минимальным числом. */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину последовательности:");
+            var sequenceLength = int.Parse(Console.ReadLine());
+            var sequence = new int[sequenceLength];
+            var minElement = int.MaxValue;
+
+            Console.WriteLine("Введите элементы последовательности, каждый с новой строки:");
+
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                sequence[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                if (minElement > sequence[i])
+                    minElement = sequence[i];
+            }
+
+            Console.WriteLine($"Минимальный элемент последовательности: {minElement}");
+            Console.ReadKey();
+        }
+    }
+}
